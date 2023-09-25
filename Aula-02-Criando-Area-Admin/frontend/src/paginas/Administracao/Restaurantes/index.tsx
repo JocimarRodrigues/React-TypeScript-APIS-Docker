ab2270bec4d5f016348a476a3f6227572ad55e82
Jocimar Rodrigues <jocimarrodrigues1337@gmail.com>
2023-09-25 15:41:38 -0300

    Listando restaurantes, usando MUI

--- Aula-02-Criando-Area-Admin/frontend/src/paginas/Administracao/Restaurantes/index.tsx
+++ Aula-02-Criando-Area-Admin/frontend/src/paginas/Administracao/Restaurantes/index.tsx
@@ -1,31 +1,3 @@
-# Uma lib muito boa para tu criar componentes de area admin no react é a MUI, o site é mui.com
-
-- Lá tem vários componentes prontos que tu pode utilizar
-- O cmd pra instalar é => npm install @mui/material @emotion/react @emotion/styled
-- Tu precisa instalar a font roboto => npm install @fontsource/roboto
-- Importar ela, ou pode pegar direto o link:
-```html
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
-/>
-```
-- O link tu pode jogar direto no index.html do teu projeto
-
-
-### TU precisa importar os links tb
-```html
-<link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/icon?family=Material+Icons"
-/>
-```
-
-# Obtendo lista da api com tsx
-
-```tsx
 import { useState, useEffect } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import {
@@ -67,7 +39,3 @@ export default function AdministracaoRestaurantes() {
     </TableContainer>
   );
 }
-
-```
-
-## Os componentes da tabela aí, todos vem da lib do MUI, basta tu ler o código na documentação lá e ir utilizando.
