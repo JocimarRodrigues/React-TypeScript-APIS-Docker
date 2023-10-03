616f97251556444a7f11e9a5ba291cf7a22974c8
Jocimar Rodrigues <jocimarrodrigues1337@gmail.com>
2023-10-03 17:26:28 -0300

    Criando o formulário de pratos, diferencial da aula é q tu aprendeu a como subir arquivos locais, usando um input

--- Aula-06-FormData-FormularioPratos/frontend/src/App.tsx
+++ Aula-06-FormData-FormularioPratos/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import AdministracaoRestaurantes from "./paginas/Administracao/Restaurantes";
 import FormularioRestaurante from "./paginas/Administracao/Restaurantes/FormularioRestaurante";
 import PaginaBaseAdmin from "./paginas/Administracao/PaginaBaseAdmin";
 import AdministracaoPratos from "./paginas/Administracao/Pratos/AdministracaoPratos";
+import FormularioPrato from "./paginas/Administracao/Pratos/FormularioPrato";
 
 function App() {
   return (
@@ -12,19 +13,11 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
       <Route path="/admin" element={<PaginaBaseAdmin />}>
-      <Route
-        path="restaurantes"
-        element={<AdministracaoRestaurantes />}
-      />
-      <Route
-        path="restaurantes/novo"
-        element={<FormularioRestaurante />}
-      />
-      <Route
-        path="restaurantes/:id"
-        element={<FormularioRestaurante />}
-      />
-      <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
       </Route>
     </Routes>
   );
